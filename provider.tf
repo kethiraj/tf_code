44f04f308c63af59c6b52705d9eb8ee28c6ba1e0
Ethiraj <ethi90@gmail.com>
2022-02-23 18:05:33 +0530

    updated

--- provider.tf
+++ provider.tf
@@ -20,22 +20,22 @@ resource "aws_security_group" "tf_course" {
   description = "Allow Standard Ports"
 
   ingress {
-    from_port  = 80
-    to_port    = 80
-    protocol   = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
-    from_port  = 443
-    to_port    = 443
-    protocol   = "tcp"
-    cidr_block = ["0.0.0.0/0"]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
-    from_port  = 0
-    to_port    = 0
-    protocol   = "-1"
-    cidr_block = ["0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags = {
